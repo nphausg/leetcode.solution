a009b112b05ab0b506449abaed13cae2efe9f517
nphausg <nphausg@gmail.com>
2025-01-20 13:21:17 +0800

    feat: 704 Binary search (kotlin)

---
+++ src/com/nphausg/leetcode/easy/BinarySearchKt.kt
@@ -0,0 +1,20 @@
+/**
+ * <a href="https://leetcode.com/problems/binary-search">704. Binary Search</a>
+ */
+class BinarySearchKt {
+    fun search(nums: IntArray, target: Int): Int {
+        var left = 0
+        var right = nums.size - 1
+        while (left <= right) {
+            val mid = left + (right - left) / 2
+            if (nums[mid] == target) {
+                return mid
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1
+    }
+}
