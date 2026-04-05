fc7e03595ef4f2ce83a983d4e0b6ff0897ddb2f6
nphausg <nphausg@gmail.com>
2026-04-05 11:29:55 +0800

    feat: add 200 NoOfIsland

---
+++ src/com/nphausg/leetcode/medium/NumberOfIslands.kt
@@ -0,0 +1,29 @@
+class Solution {
+    fun numIslands(grid: Array<CharArray>): Int {
+        var count = 0
+        for (r in grid.indices){
+            for (c in grid[0].indices){
+                if(grid[r][c] == '1'){
+                    count ++
+                    dfs(grid, r, c)
+                }
+            }
+        }
+        return count
+    }
+
+    fun dfs (grid: Array<CharArray>, r : Int, c : Int){
+        val rows = grid.size
+        val cols = grid[0].size
+        if (r < 0 || r >= rows || c < 0 || c >= cols || grid[r][c] == '0'){
+            return
+        }
+
+        grid[r][c] = '0'
+
+        dfs (grid, r + 1, c)
+        dfs (grid, r - 1, c)
+        dfs (grid, r, c + 1)
+        dfs (grid, r, c - 1)
+    }
+}
