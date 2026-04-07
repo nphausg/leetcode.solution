4f61bd8592e3725a9f85a6b8a0f8b39d47426d41
nphausg <nphausg@gmail.com>
2026-04-07 11:29:57 +0800

    feat: 1091. Shortest Path in Binary Matrix

--- src/com/nphausg/leetcode/easy/FirstUniqueCharacterInAStringKt.kt
+++ src/com/nphausg/leetcode/easy/FirstUniqueCharacterInAStringKt.kt
@@ -6,6 +6,23 @@ class FirstUniqueCharacterInAStringKt {
         val charCount = input.groupingBy { it }.eachCount()
         return input.find { charCount[it] == 1 }
     }
-//    val result = firstNonRepeatedChar("swiss")
-//    println(result)  // Output: w
+
+    fun firstUniqChar(s: String): Int {
+        val charCounts = s.groupingBy { it }.eachCount()
+        return s.indexOfFirst { charCounts[it] == 1 }
+    }
+
+    fun firstUniqChar2(s: String): Int {
+        var count = IntArray(26)
+        for (c in s){
+            count[c - 'a']++
+        }
+        for (i in s.indices){
+            if(count[s[i] - 'a'] == 1){
+                return i
+            }
+        }
+        return -1
+    }
+
 }
